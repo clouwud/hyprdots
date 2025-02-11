4028e01afad5212d6cf7b95dbd44afc3722c2a4a
clouwud <dakshgajbhiye902@gmail.com>
2025-02-11 23:35:30 +0530

    init

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,8 @@
+c.fonts.default_family = 'JetBrainsMono Nerd Font'
+c.fonts.default_size= '12pt'
+c.fonts.tabs.selected = '15pt default_family'
+c.fonts.tabs.unselected = '15pt default_family'
+
+config.load_autoconfig()
+config.set("colors.webpage.darkmode.enabled", True)
+config.source('/home/daksh/.config/qutebrowser/themes/gruvbox.py')
