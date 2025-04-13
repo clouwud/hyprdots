e5852ac8665cabf38d7d1da251b0760114b84b56
clouwud <dakshgajbhiye902@gmail.com>
2025-04-13 14:50:15 +0530

    init

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,4 +1,28 @@
-require("full-border"):setup {
+require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.PLAIN,
-}
+	type = ui.Border.ROUNDED,
+})
+
+-- require("simple-status"):setup()
+require("yaziline"):setup({
+	color = "#98c379", -- main theme color
+	default_files_color = "darkgray", -- color of the file counter when it's inactive
+	selected_files_color = "white",
+	yanked_files_color = "green",
+	cut_files_color = "red",
+
+	separator_style = "liney", -- "angly" | "curvy" | "liney" | "empty"
+	-- separator_open = "",
+	-- separator_close = "",
+	-- separator_open_thin = "",
+	-- separator_close_thin = "",
+	-- separator_head = "",
+	-- separator_tail = "",
+
+	select_symbol = "",
+	yank_symbol = "󰆐",
+
+	filename_max_length = 24, -- truncate when filename > 24
+	filename_truncate_length = 6, -- leave 6 chars on both sides
+	filename_truncate_separator = "...", -- the separator of the truncated filename
+})
